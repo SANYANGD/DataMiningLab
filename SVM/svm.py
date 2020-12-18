80a08e04c106c87bc793aecc48b262398b96c989
ChunyangDai <chunyangdai@outlook.com>
2020-12-18 22:39:01 +0800

    svm

--- SVM/svm.py
+++ SVM/svm.py
@@ -2,5 +2,5 @@ import numpy as np
 
 filepath = 'dataset.csv'
 data = np.loadtxt(filepath, dtype=np.float, delimiter=',', usecols=range(12), encoding='utf-8')
-data_tag = np.loadtxt(filepath, dtype=np.float, delimiter=',', usecols=12, encoding='utf-8')
-print(data_tag)
+data_tag = np.loadtxt(filepath, dtype=np.int, delimiter=',', usecols=12, encoding='utf-8')
+print(data)
